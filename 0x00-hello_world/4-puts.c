6cf1b77bcca07fe8bb77cc8252b027f1ec41fb74
nehadadel <eng.nehadadel@gmail.com>
2023-08-10 14:42:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,10 @@
  */
 #include <stdio.h>
 /**
- * main function - This is printing "Programming is like building a multilingual puzzle"
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
