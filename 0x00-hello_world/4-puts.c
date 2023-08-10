b88a1f3fc7832acb3157e5724917abd6a147038f
nehadadel <eng.nehadadel@gmail.com>
2023-08-10 14:19:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
+/*
+ * File: 4-puts.c
+ */
 #include <stdio.h>
+/* prints ""Programming is like building a multilingual puzzle"
+ *return always 0
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
