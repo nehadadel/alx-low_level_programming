a08f518050be382ed2aba17ef4948db038d98ccf
nehadadel <eng.nehadadel@gmail.com>
2023-08-11 19:20:37 -0700

    9-comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description : the program c prints single numbers
+ *
+ *Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i;
@@ -6,8 +13,11 @@ int i;
 for (i = 0 ; i <= 9 ; i++)
 {
 putchar('0' + i);
+if (i < 9)
+{
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
