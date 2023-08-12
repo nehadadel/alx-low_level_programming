f88f40e4dbcb05a72cdf53637ca952973090f843
nehadadel <eng.nehadadel@gmail.com>
2023-08-11 19:53:52 -0700

    101-comb3

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description : the program c prints
+ *
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+
+int j;
+
+int k;
+
+for (k = 0 ; k < 8 ; k++)
+{
+for (j = k + 1 ; j < 9 ; j++)
+{
+for (i = j + 1 ; i <= 9 ; i++)
+{
+putchar('0' + j);
+putchar('0' + i);
+if ((j == 8) && (i == 9) && (k == 7))
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
