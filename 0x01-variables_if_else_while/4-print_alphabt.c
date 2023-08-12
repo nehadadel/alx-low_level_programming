2d8aa62b6f918d67be1233ef35bdad020cc0a74c
nehadadel <eng.nehadadel@gmail.com>
2023-08-11 17:43:25 -0700

    print lowercase expect e ,q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description : the program c prints the alphabet in lowercase except q and e
+ *
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if ((ch != 'e')&&(ch != 'q'))
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
