6d44ee7bf7526cf013de2d36792841b6ff343a21
nehadadel <eng.nehadadel@gmail.com>
2023-08-15 07:26:27 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
  *main -  prints _putchar, followed by a new line
  *
  *Return: Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
 _putchar('_');
 _putchar('p');
