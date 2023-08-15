3337246e35d3c4e05b7f156e8d56448575b084fa
nehadadel <eng.nehadadel@gmail.com>
2023-08-15 16:15:54 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ *_abs - that return absolute value.
+ *
+ *@i: the integer to be converted
+ *
+ *Return: absolute value of i
+ */
+int _abs(int i)
+{
+if (i < 0)
+{
+i = i * -1;
+}
+return (i);
+}
