15af78f78283ccb89d26121859799ce9ab2e7705
nehadadel <eng.nehadadel@gmail.com>
2023-08-15 18:32:30 -0700

    hr:m

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *jack_bauer - prints time table in this format: HH:MM
+ *
+ *Return: nothing
+*/
+void jack_bauer(void)
+{
+int hr;
+
+int mintus;
+
+for (hr = 0; hr <= 23; hr++)
+{
+for (mintus = 0 ; mintus <= 59 ; mintus ++)
+{
+_putchar('0' + (hr / 10));
+_putchar('0' + (hr % 10));
+_putchar(':');
+_putchar('0' + (mintus / 10));
+_putchar('0' + (mintus % 10));
+_putchar('\n');
+}
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void jack_bauer(void);
 int print_last_digit(int i);
 int _putchar(char ch);
 void print_alphabet(void);
