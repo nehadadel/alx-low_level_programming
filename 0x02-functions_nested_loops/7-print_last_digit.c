15c327c72e17c7603cd551fb0cab7bfadef515ba
nehadadel <eng.nehadadel@gmail.com>
2023-08-15 17:27:14 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,11 @@ int print_last_digit(int i)
 {
 int last_digit;
 
-int j;
-
-j = i;
-if (j < 0)
+last_digit = i % 10;
+if (last_digit < 0)
 {
-j = -1 * j;
+last_digit = last_digit * -1;
 }
-last_digit = j % 10;
 _putchar(last_digit + '0');
 return (last_digit);
 }
