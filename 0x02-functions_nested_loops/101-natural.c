114fbeb7a0125f9610f6be24dd25e91d8ef88eb5
nehadadel <eng.nehadadel@gmail.com>
2023-08-16 17:22:35 -0700

    task13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,9 +11,9 @@ int i, sum;
 sum = 0;
 for (i = 0 ; i < 1024 ; i++)
 {
-if((i % 3 == 0) || (i % 5 == 0))
+if ((i % 3 == 0) || (i % 5 == 0))
 {
-sum = sum + i ;
+sum = sum + i;
 }
 }
 printf("%d\n", sum);
