61abe53b33511c253d53d6d2bb7a9563579eeb80
nehadadel <eng.nehadadel@gmail.com>
2023-08-17 16:00:43 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_numbers - prints the numbers, from 0 to 9
+ *
+ *Return: nothing
+*/
+void print_numbers(void)
+{
+for (i = 0; i < 10; i++)
+{
+_putchar('0' + i);
+}
+_putchar('\n');
+}
