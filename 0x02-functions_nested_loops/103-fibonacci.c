b54fcf708e32ae946fe2758d709c67697b054b85
nehadadel <eng.nehadadel@gmail.com>
2023-08-16 18:06:07 -0700

    task15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,30 +6,23 @@
 */
 int main(void)
 {
-long int first, second, next;
+long int first, second, next, even;
 int count;
 
 first = 1;
 second = 2;
 next = first + second;
-printf("%lu, ", first);
-printf("%lu, ", second);
+even = 2;
 for (count = 3; count <= 50; count++)
 {
-if(next <= 4000000 && next % 2 == 0)
+if (next <= 4000000 && next % 2 == 0)
 {
-if (count == 50)
-{
-printf("%lu\n", next);
-}
-else
-{
-printf("%lu, ", next);
-}
+even = even + next;
 }
 first = second;
 second = next;
 next = first + second;
 }
+printf("%lu", even);
 return (0);
 }
