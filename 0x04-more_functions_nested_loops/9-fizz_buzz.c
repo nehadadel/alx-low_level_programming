01f29473eb5147865fdbff35dfb24f9f79449021
nehadadel <eng.nehadadel@gmail.com>
2023-08-17 18:08:23 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,13 +8,13 @@ int main(void)
 {
 int i;
 
-for (i = 0; i <= 100; i++)
+for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i != 0)
+if (i % 3 == 0)
 {
 printf("Fizz");
 }
-if (i % 5 == 0 && i != 0)
+if (i % 5 == 0)
 {
 printf("Buzz");
 }
