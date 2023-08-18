36cda0a5737735bb7d63f61ab8ed1a4fe50b693b
nehadadel <eng.nehadadel@gmail.com>
2023-08-17 17:30:25 -0700

    task8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_square - prints a square.
+ *
+ *@size: integer number
+ *
+ *Return: Nothing
+*/
+void print_square(int size)
+{
+if (size > 0)
+{
+int i, j;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
