8850f402d099269e7a961af53ff69a7b78a192a0
nehadadel <eng.nehadadel@gmail.com>
2023-08-17 18:59:23 -0700

    task10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,29 +8,23 @@
 */
 void print_triangle(int size)
 {
+int inc1, inc2;
+
 if (size > 0)
 {
-int i, j, k;
-
-for (i = 1; i <= size; i++)
+for (inc1 = 1; inc1 <= size; inc1++)
 {
-for (j = size - i; j > 0; j--)
-{
+for ((inc2 = size - inc1); inc2 > 0; inc2--)
 _putchar(' ');
-}
-for (k = 0; k < i; k++)
-{
+
+for (inc2 = 0; inc2 < inc1; inc2++)
 _putchar('#');
-}
-if (i == size)
-{
+
+if (inc1 == size)
 continue;
-}
+
 _putchar('\n');
 }
 }
-else
-{
 _putchar('\n');
-}
 }
