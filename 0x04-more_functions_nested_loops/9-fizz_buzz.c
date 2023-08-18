277ab919272179db1bc58ac80c86f8d3860b0b6a
nehadadel <eng.nehadadel@gmail.com>
2023-08-17 17:50:38 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - prints number from 0-100
+ *
+ *Return: (0)
+*/
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz");
+}
+if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+putchar(' ')
+}
+putchar('\n');
+return (0);
+}
