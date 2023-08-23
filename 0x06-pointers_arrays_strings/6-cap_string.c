f1528c1d6bfd0f6311ab6d0ba332bd326bf3aaca
nehadadel <eng.nehadadel@gmail.com>
2023-08-23 14:27:14 -0700

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,63 +7,63 @@
  */
 char *cap_string(char *s)
 {
-  int index;
-  int referance;
+int index;
+int referance;
 
-  referance = 0;
+referance = 0;
 
-  index = 0;
-  while (*(s + index) != '\0')
-    {
-      referance = 0;
-      switch (*(s + index))
-	{
-	case ' ' :
-	referance = 1;
-       break;
-    case '"':
-      referance = 1;
-       break;
-    case '.' :
-      referance = 1;
-       break;
-    case ':' :
-      referance = 1;
-       break;
-    case '\t' :
-      referance = 1;
-       break;
-    case ',' :
-      referance = 1;
-       break;
-    case '{' :
-      referance = 1;
-       break;
-    case '}' :
-      referance = 1;
-       break;
-    case '(' :
-      referance = 1;
-       break;
-    case ')' :
-      referance = 1;
-       break;
-    case '\n' :
-      referance = 1;
-    break;
-    case '!' :
-      referance = 1;
-      break;
-    default :
-      referance = 0;
-    }
-      if (referance == 1 && *(s + index + 1) >= 97 && *(s + index + 1) <= 122)
-	{
-	  *(s + index + 1) = *(s + index + 1) - 32;
-	}
-      index++;
-	 
-    }
-  return (s);  
+index = 0;
+while (*(s + index) != '\0')
+{
+referance = 0;
+switch (*(s + index))
+{
+case ' ' :
+referance = 1;
+break;
+case '"':
+referance = 1;
+break;
+case '.' :
+referance = 1;
+break;
+case ':' :
+referance = 1;
+break;
+case '\t' :
+referance = 1;
+break;
+case ',' :
+referance = 1;
+break;
+case '{' :
+referance = 1;
+break;
+case '}' :
+referance = 1;
+break;
+case '(' :
+referance = 1;
+break;
+case ')' :
+referance = 1;
+break;
+case '\n' :
+referance = 1;
+break;
+case '!' :
+referance = 1;
+break;
+default :
+referance = 0;
+}
+if (referance == 1 && *(s + index + 1) >= 97 && *(s + index + 1) <= 122)
+{
+*(s + index + 1) = *(s + index + 1) - 32;
+}
+index++;
+ 
+}
+return (s);  
 }    
   
