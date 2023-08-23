26819bdd1a5109be9198b3d706e9c8c62635f8e6
nehadadel <eng.nehadadel@gmail.com>
2023-08-23 10:26:20 -0700

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,6 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 int index;
 
 for (index = 0; *(src + index) != '\0' && index < n; index++)
+{
 *(dest + index) = *(src + index);
+}
+for ( ; index < n; index++)
+*(dest + index) = '\0';
 return (dest);
 }
