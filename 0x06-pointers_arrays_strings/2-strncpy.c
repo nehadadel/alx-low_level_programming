4342308de382be12fde42776b68e01f0985841be
nehadadel <eng.nehadadel@gmail.com>
2023-08-23 09:39:43 -0700

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - concatenates 2 strings.
+ *
+ *@dest: string with concatenation
+ *@src: string to be concatenated
+ *@n: integer number
+ *
+ * Return: pointer to ch.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int index;
+
+for (index = 0; *(src + index) != '\0' && i < n; i++)
+*(dest + index) = *(scr + index);
+return (dest);
+}
