77f01e6b39e93a0ba0396357151729be2e50f9f8
nehadadel <eng.nehadadel@gmail.com>
2023-08-24 15:25:50 -0700

    101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,6 +8,11 @@
 */
 void print_number(int n)
 {
+if (n < 0)
+{
+_putchar('-');
+n = n * (-1);
+}
 if (n >= 0 && n <= 9)
 _putchar('0' + n);
 if (n >= 10 && n <= 99)
@@ -28,4 +33,5 @@ _putchar('0' + (n % 1000) / 100);
 _putchar('0' + ((n % 1000) % 100) / 10);
 _putchar('0' + ((n % 1000) % 100) % 10);
 }
+_putchar('\n');
 }
