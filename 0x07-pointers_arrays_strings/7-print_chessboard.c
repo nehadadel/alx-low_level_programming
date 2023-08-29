7106f3e163574248f596ae1a52214653fe9859c5
nehadadel <eng.nehadadel@gmail.com>
2023-08-29 15:26:07 -0700

    task6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,14 +7,13 @@ int i, j;
 i = 0;
 j = 0;
 
-
-while (*(a + i) != '\0')
+ for (i = 0; i < 8; i++)
 {
-for (j = 0; j < 8 ; j++)
+for (j = 0; j < 8; j++)
 {
-_putchar((*(a + i))[j]);
+_putchar(a[i][j]);
 }
-i++;
+
 }
   
 }
