49d7c1ebf1f98f9e719ea59d12abe8b4aa05e8b3
nehadadel <eng.nehadadel@gmail.com>
2023-08-29 15:29:17 -0700

    task6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,9 @@
 #include "main.h"
-/***/
+/**
+ *print_chessboard - print cheessboard
+ *
+ *@a: array of char
+*/
 void print_chessboard(char (*a)[8])
 {
 int i, j;
@@ -7,13 +11,11 @@ int i, j;
 i = 0;
 j = 0;
 
- for (i = 0; i < 8; i++)
+for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
 {
 _putchar(a[i][j]);
 }
-
 }
-  
 }
