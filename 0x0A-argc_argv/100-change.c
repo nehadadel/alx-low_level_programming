6471c3bbd0825f93ace65baf2728cdafefefcfae
nehadadel <eng.nehadadel@gmail.com>
2023-09-01 18:38:03 -0700

    task10

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,16 +11,44 @@
 
 int main(int argc, char *argv[])
 {
-int coins;
+int coins, cents;
 
+ coins = 0;
 if (argc < 2)
 {
 printf("Error\n");
 return (1);
 }
-coins = atoi(*(argv + 1));
-if (coins < 0)
+cents = atoi(*(argv + 1));
+if (cents < 0)
 printf("0\n");
+else
+{
+while (cents >= 25)
+{
+cents = cents - 25;
+coins++;
+}
+while (cents >= 10)
+{
+cents = cents - 10;
+coins++;
+}
+while (cents >= 5)
+{
+cents = cents - 5;
+coins++;
+}
+while (cents >= 2)
+{
+cents = cents - 2;
+coins++;
+}
+if (cents == 1)
+coins++;
+printf("%d\n",coins);
+}
+
 return (0);
 
 }
