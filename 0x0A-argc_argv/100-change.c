5bd1504e447b1fd0cac74d69113949cd40530230
nehadadel <eng.nehadadel@gmail.com>
2023-09-01 18:07:17 -0700

    task10

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main -  prints the minimum number of coins
+ *to make change for an amount of money.
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+
+return (0);
+
+}
