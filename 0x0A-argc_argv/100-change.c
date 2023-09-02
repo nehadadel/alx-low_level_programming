7b0fc7817f02d3606653daf4c418273aa4e9b7e8
nehadadel <eng.nehadadel@gmail.com>
2023-09-01 18:20:09 -0700

    task10

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,12 +14,13 @@ int main(int argc, char *argv[])
 int coins;
 
 if (argc < 2)
-{printf("Error\n");
+{
+printf("Error\n");
 return (1);
 }
 coins = atoi(*(argv + 1));
 if (coins < 0)
-printf ("0\n");
+printf("0\n");
 
 return (0);
 
