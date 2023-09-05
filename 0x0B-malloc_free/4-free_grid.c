3b52d8066fa84638638c74f7e46b781460bc6191
nehadadel <eng.nehadadel@gmail.com>
2023-09-05 14:29:30 -0700

    task4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,12 +10,9 @@
 */
 void free_grid(int **grid, int height)
 {
-int i, j, width;
-width = 0;
-while (grid[0])
-width++;
+int i;
+
 for (i = 0; i < height ; i++)
-for (j = 0; j < width; j++)
-free(grid[i][j]);
+free(grid[i]);
 free(grid);
 }
