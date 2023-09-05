5c9e5206b3e93bc190c24c9935d9438b5ea52c5e
nehadadel <eng.nehadadel@gmail.com>
2023-09-05 12:52:10 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,22 +12,20 @@
 int **alloc_grid(int width, int height)
 {
 int i, j;
- int** arr;
+int* arr[width];
  
 if (width <= 0 || height <= 0)
 return (NULL);
-*arr = (int**)malloc(width * sizeof(int*));
-if (*arr == NULL)
-return (NULL);
-for (i = 0; i < width; i++)
+
+for (i = 0; i < height; i++)
 {
-arr[i] = (int*)malloc(height * sizeof(int));
+arr[i] = (int*)malloc(width * sizeof(int));
 if (arr[i] == NULL)
 return (NULL);
 }
-for (i = 0; i < width; i++)
-for (j = 0; j < height; j++)
+for (i = 0; i < height; i++)
+for (j = 0; j < width; j++)
 arr[i][j] = 0;
-return (arr);
 
+return (arr);
 }
