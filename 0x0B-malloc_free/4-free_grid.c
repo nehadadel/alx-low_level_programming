3619ef515c30310623d496c5a38f634271a31f92
nehadadel <eng.nehadadel@gmail.com>
2023-09-05 14:30:34 -0700

    task4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- *free_grid -  frees a 2 dimensional grid previously created by 
+ *free_grid -  frees a 2 dimensional grid previously created by
  *alloc_grid function.
  *
  *@grid: 2d arry
