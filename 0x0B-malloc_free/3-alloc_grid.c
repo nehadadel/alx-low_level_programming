3b99df9c3d8b7770484095e81efaacb0d78d72e6
nehadadel <eng.nehadadel@gmail.com>
2023-09-05 13:11:37 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,10 @@ for (i = 0; i < height; i++)
 {
 arr[i] = (int*)malloc(width * sizeof(int));
 if (arr[i] == NULL)
+{
+free (arr);
 return (NULL);
+}
 }
 for (i = 0; i < height; i++)
 for (j = 0; j < width; j++)
