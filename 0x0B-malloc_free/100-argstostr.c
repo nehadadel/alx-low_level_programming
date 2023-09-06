7ef556e692cab8643c1a91cf377341078af2c285
nehadadel <eng.nehadadel@gmail.com>
2023-09-06 14:00:51 -0700

    task5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,36 +11,38 @@
 */
 char *argstostr(int ac, char **av)
 {
-int i, len, j;
-char **arr;
+int i, k, len, j;
+char *arr;
 
 if (ac == 0 || av == NULL)
 return(NULL);
 
-arr = (char **)malloc(ac * sizeof(char *));
-if (arr == NULL)
+len = 0;
+k = 0;
+for (i = 0; i < ac; i++)
+{
+j = 0;
+while (av[i][j] != '\0')
+{
+j++
+len++;
+}
+len++;
+}
+
+arr = (char *)malloc(len + ac  * sizeof(char));
+if (arr = NULL)
 return (NULL);
 
 for (i = 0; i < ac; i++)
 {
-len = 0;
-while (*av[i] != '\0')
-len++;
-arr[i] = (char *)malloc(len + 1 * sizeof(char));
-if (arr[i] == NULL)
-return (NULL);
-
- 
+for (j = 0; av[i][j] = '\0'; j++)
+{
+arr[k] = av[i][j];
+k++;
 }
-for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-		  arr[i][j] = av[i][j];
-		}
-		arr[i][j] = '\n';	       
+arr[k] = '\n';
+k++;
 }
-
-
-return(*arr);
+ return (arr);
 }
