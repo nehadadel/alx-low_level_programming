cf37e86c61e6947ce04165095ff00811ece2638f
nehadadel <eng.nehadadel@gmail.com>
2023-09-06 11:22:10 -0700

    task5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/***/
+/**
+ *argstostr - concatenates all the arguments of your program.
+ *
+ *@ac: integer number
+ *@av: pointer to pointer char
+ *
+ *Return: char to pointer
+*/
 char *argstostr(int ac, char **av)
 {
 int i;
@@ -14,7 +21,7 @@ arr = (char **)malloc(ac * sizeof(char *));
 if (arr == NULL)
 return (NULL);
 
- for (i = 0; i < ac; i++)
-   arr[i] = av[i];
- return(*arr);
+for (i = 0; i < ac; i++)
+arr[i] = av[i];
+return(*arr);
 }
