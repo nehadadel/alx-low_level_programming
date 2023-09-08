f110831a28ec3447d341849d86764d542ab84ec8
nehadadel <eng.nehadadel@gmail.com>
2023-09-07 19:46:15 -0700

    task5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,7 +11,7 @@
 */
 int main(int argc, char *argv[])
 {
-int i, j;
+int i, j, num1, num2;
 
 if (argc != 3)
 {
@@ -27,5 +27,9 @@ printf("Error\n");
 exit(98);
 }
 }
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+printf("%d\n",num1 * num2);
 return (0);
 }
