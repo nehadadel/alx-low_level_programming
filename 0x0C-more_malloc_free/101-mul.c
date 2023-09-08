c08ae266cd1c77283d18d7bda69a2227df470a08
nehadadel <eng.nehadadel@gmail.com>
2023-09-07 19:32:33 -0700

    task5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - multiplies two positive numbers.
+ *
+ *@argc: integer number
+ *@argv: pointr to arry
+ *
+ *Return: 0 if success
+*/
+int main(int argc, char *argv[])
+{
+
+return (0);
+}
