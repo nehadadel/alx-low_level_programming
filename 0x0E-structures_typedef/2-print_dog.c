a6e8649df0c333e060ea3b8b25f8236a2ee9893f
nehadadel <eng.nehadadel@gmail.com>
2023-09-11 14:41:35 -0700

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,12 +13,12 @@ if (d != NULL)
 if (d->name != NULL)
 printf("Name: %s\n", d->name);
 else
-printf("Name: (nil)");
+printf("Name: (nil)\n");
 
 printf("Age: %f\n", d->age);
 if (d->owner != NULL)
 printf("Owner: %s\n", d->owner);
 else
-printf("Owner: (nil)");
+printf("Owner: (nil)\n");
 }
 }
