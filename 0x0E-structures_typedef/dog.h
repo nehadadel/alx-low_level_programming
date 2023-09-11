6dfd4e25beacc2f8c28ba0300834225b9f3f5afa
nehadadel <eng.nehadadel@gmail.com>
2023-09-11 13:56:22 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
-  char *name;
-  float age;
-  char *owner;
+/**
+ *struct dog - create dog struct
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of the dog
+ *
+ *Description: This struct stores name, age, and the owner of dog
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
 #endif
