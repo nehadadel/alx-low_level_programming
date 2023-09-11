5b2a7bf5e24d48d57785be63f13c8ba7afe73b2e
nehadadel <eng.nehadadel@gmail.com>
2023-09-11 09:50:59 -0700

    task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints name of file.
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+
+}
