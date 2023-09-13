537020f9fd370fe239f2bfbe90134d8c7e20dfe2
nehadadel <eng.nehadadel@gmail.com>
2023-09-13 10:59:04 -0700

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  *print_name -  prints a name.
  *
@@ -7,5 +8,6 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
+if (f != NULL)
 f(name);
 }
