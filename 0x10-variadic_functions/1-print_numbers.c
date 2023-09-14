f5e8a1b25c883bfce483d720a3c9c30b32cfd96f
nehadadel <eng.nehadadel@gmail.com>
2023-09-14 15:17:47 -0700

    task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,10 +16,10 @@ va_list args;
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
-if (separator != NULL)
-printf("%d%s", va_arg(args, int), separator);
-else
+if (separator == NULL || i == n - 1)
 printf("%d", va_arg(args, int));
+else
+printf("%d%s", va_arg(args, int), separator);
 }
 printf("\n");
 va_end(args);
