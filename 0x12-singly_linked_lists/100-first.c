c1851e9d917591f37e98d84e9a476045d75f658c
nehadadel <eng.nehadadel@gmail.com>
2023-09-21 18:17:55 -0700

    task5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
-void __attribute__((constructor)) before(void);
-
 /**
-*beore - Prints a string before the
-* main function is executed.
-*/
+ * before - Prints a string before the
+ * main function is executed.
+ */
+void __attribute__((constructor)) before(void)
 void before(void)
 {
 printf("You're beat! and yet, you must allow,\n"
