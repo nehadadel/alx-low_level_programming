2d2c78f38c7734c638f18e6bde834002f5841f1c
nehadadel <eng.nehadadel@gmail.com>
2023-09-21 18:23:36 -0700

    task5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,13 @@
-#include<stdio.h>
-void before_main() _attribute_((constructor));
-void before_main()
-{
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
+#include <stdio.h>
 
+void __attribute__((constructor)) before_before(void);
+
+/**
+ * before_before - Prints a string before the
+ *   main function is executed.
+ */
+void before_before(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
