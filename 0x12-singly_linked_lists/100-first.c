8670e8c03a4bdba5bca56ef88ad5a26ae1935153
nehadadel <eng.nehadadel@gmail.com>
2023-09-21 18:10:38 -0700

    task5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-void before_main() __attribute__((constructor));
-void after_main() __attribute__((destructor));
-void before_main()
+void before_main(void) __attribute__((constructor));
+void after_main(void) __attribute__((destructor));
+void before_main(void)
 {
 printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
