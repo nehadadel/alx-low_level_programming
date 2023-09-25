3455cc5eb46c81ff7a83cfbc9f250f8acc474f23
nehadadel <eng.nehadadel@gmail.com>
2023-09-25 15:38:45 -0700

    task5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,9 @@ void free_listint2(listint_t **head)
 listint_t *temp;
 while (*head != NULL)
 {
-temp = *head->next;
+temp = (*head)->next;
 free(*head);
 *head = temp;
 }
-*head =NULL;
+*head = NULL;
 }
