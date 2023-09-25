289ec2f68d2c29a1e5c7845bf63c69dcd347d8f7
nehadadel <eng.nehadadel@gmail.com>
2023-09-25 13:52:59 -0700

    task3

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,20 +7,17 @@
  *
  *Return: pointer to new_node
 */
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-const listint_t temp = head;
 listint_t *new_node;
 
-new_node = (listint_t) malloc(sizeof(listint_t *);
+new_node = malloc(sizeof(listint_t));
 if (new_node == NULL)
 return (NULL);
 
 
-temp = temp->next;
-head->next = new_node;
-new_node->next = temp;
+new_node->next = *head;
 new_node->n = n;
-
+new_node = *head;
 return (new_node);
 }
