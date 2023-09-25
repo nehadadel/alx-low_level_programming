149273c9d1bcd1460c1ce890e140a3aa6770eb75
nehadadel <eng.nehadadel@gmail.com>
2023-09-25 15:33:29 -0700

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *free_listint - Frees a list
+ *
+ *@head: A pointer to the list head
+ *
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+while (*head != NULL)
+{
+temp = *head->next;
+free(*head);
+*head = temp;
+}
+*head =NULL;
+}
