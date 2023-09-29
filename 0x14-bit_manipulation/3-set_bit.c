4674a6f672f5b6f77cb2c4e0419982640d8a61a9
nehadadel <eng.nehadadel@gmail.com>
2023-09-28 17:54:50 -0700

    t4

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *set_bit - sets the value of a bit to 1 at a given index.
+ *
+ *@n: long number
+ *@index: is the index, starting from 0 of the bit you want to set
+ *
+ *Return: 1 if it worked, or -1 if an error occurred
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int num = 0x01;
+
+if (n == NULL || index > 32)
+return (-1);
+
+num = 0x01 << index;
+*n = *n | num;
+return(1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stddef.h>
 
+int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char ch);
 unsigned int binary_to_uint(const char *b);
