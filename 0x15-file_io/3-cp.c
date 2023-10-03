20f0351bd1a0795b90852509159ec22706fad696
nehadadel <eng.nehadadel@gmail.com>
2023-10-02 17:38:38 -0700

    t3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,13 +18,15 @@ int main(int argc, char *argv[])
 int fd_from, fd_to;
 char buffer[BUFFER_SIZE];
 ssize_t bytes_read, bytes_written;
-
+const char *file_from;
+const char *file_to;
+ 
 if (argc != 3)
 error_exit("Usage: cp file_from file_to", 97);
 
 
-const char *file_from = argv[1];
-const char *file_to = argv[2];
+file_from = argv[1];
+file_to = argv[2];
 
 fd_from = open(file_from, O_RDONLY);
 if (fd_from == -1)
