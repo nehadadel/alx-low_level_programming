85df53dd5f9a10608254ef1849af05b4a94df075
nehadadel <eng.nehadadel@gmail.com>
2023-11-09 15:22:28 -0800

    task0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ *print_dlistint - prints all the elements of a list
+ *
+ *@h: pointer to head of linked list
+ *
+ *Return: number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+const listint_t *temp = h;
+size_t counter = 0;
+if (h == NULL)
+return (0);
+
+while (temp)
+{
+printf("%d\n", temp->n);
+temp = temp->next;
+counter++;
+}
+return (counter);
+}
