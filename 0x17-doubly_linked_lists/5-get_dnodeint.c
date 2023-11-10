3799875bdba6f32b1afa5fad9ed62166b6bdf0af
nehadadel <eng.nehadadel@gmail.com>
2023-11-09 17:39:30 -0800

    task3

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,7 +7,7 @@
  *
  *Return:  returns the nth node of a listint_t linked list.
 */
-dlistint_t *get_dnodeint_at_index(listint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 unsigned int len = 0;
 dlistint_t *temp = head;
