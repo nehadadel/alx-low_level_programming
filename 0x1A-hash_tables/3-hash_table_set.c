32e0e26aeb9e67f530a824d449cbe1e5d7cefa14
nehadadel <eng.nehadadel@gmail.com>
2023-12-01 15:17:52 -0800

    t3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,39 +10,40 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-  hash_node_t *new_node;
-  unsigned long int index;
-  char *dup_value, *dup_key;
-  if (ht == NULL || key == NULL)
-    return (0);
-  new_node = malloc(sizeof(hash_node_t *));
-  if (new_node == NULL)
-    return (0);
-  dup_key = strdup(key);
-  if (dup_key == NULL)
-    {
-      free(new_node);
-      return (0); }
-  if (value != NULL)
-    {
-      dup_value = strdup(value);
-      if (dup_value == NULL)
-	{
-	  free(new_node);
-	  free(dup_key);
-	  return(0); } }
-  else
-    dup_value = NULL;
-  new_node->key = dup_key;
-  new_node->value = dup_value;
-  index = key_index((unsigned char *)dup_key, ht->size);
+hash_node_t *new_node;
+unsigned long int index;
+char *dup_value, *dup_key;
 
-  if (value != NULL)
-    dup_value = strdup(value);
-  else
-    dup_value = NULL;
+if (ht == NULL || key == NULL)
+return (0);
+new_node = malloc(sizeof(hash_node_t *));
+if (new_node == NULL)
+return (0);
+dup_key = strdup(key);
+if (dup_key == NULL)
+{
+free(new_node);
+return (0); }
+if (value != NULL)
+{
+dup_value = strdup(value);
+if (dup_value == NULL)
+{
+free(new_node);
+free(dup_key);
+return(0); } }
+else
+dup_value = NULL;
+new_node->key = dup_key;
+new_node->value = dup_value;
+index = key_index((unsigned char *)dup_key, ht->size);
 
-  new_node->next = ht->array[index];
-  ht->array[index] = new_node;
-  return (1);
+if (value != NULL)
+dup_value = strdup(value);
+else
+dup_value = NULL;
+
+new_node->next = ht->array[index];
+ht->array[index] = new_node;
+return (1);
 }
