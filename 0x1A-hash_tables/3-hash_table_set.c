dbec453684c587ed9777576f303c83c7a3435f6d
nehadadel <eng.nehadadel@gmail.com>
2023-12-01 17:55:42 -0800

    t3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,17 @@
 #include "hash_tables.h"
 /**
+ * free_node - Free a node.
+ * @node: Node to free.
+ *
+ * Return: Void.
+ */
+void free_node(hash_node_t *node)
+{
+free(node->key);
+free(node->value);
+free(node);
+}
+/**
 *hash_table_set - that adds an element to the hash table
 *
 *@ht: hash table
