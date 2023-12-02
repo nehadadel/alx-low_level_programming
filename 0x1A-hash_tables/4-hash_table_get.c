6d7dbce9ac5877ca3c97d6cc6b8cedec4c931976
nehadadel <eng.nehadadel@gmail.com>
2023-12-01 18:26:08 -0800

    t3

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,13 +10,13 @@
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-hash_node_t *tmp = ht->array[index];
+hash_node_t *tmp;
 unsigned long int index;
 if (ht == NULL || key == NULL)
 return (NULL);
 
 index = key_index((unsigned char *)key, ht->size);
-
+tmp = ht->array[index];
 if (ht->array[index] == NULL)
 return (NULL);
 
