bff15d082dcc33979d3ab161e8e3ef2e4d0d8957
nehadadel <eng.nehadadel@gmail.com>
2023-12-01 17:53:13 -0800

    t3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,17 +1,5 @@
 #include "hash_tables.h"
 /**
- * free_node - Free a node.
- * @node: Node to free.
- *
- * Return: Void.
- */
-void free_node(hash_node_t *node)
-{
-free(node->key);
-free(node->value);
-free(node);
-}
-/**
 *hash_table_set - that adds an element to the hash table
 *
 *@ht: hash table
@@ -22,7 +10,7 @@ free(node);
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-  hash_node_t *new_node, *tmp;
+hash_node_t *new_node, *tmp;
 unsigned long int index;
 char *dup_value, *dup_key;
 
@@ -34,54 +22,31 @@ return (0);
 dup_key = strdup(key);
 if (dup_key == NULL)
 {
-free(new_node);
+free_node(new_node);
 return (0); }
 if (value != NULL)
 {
 dup_value = strdup(value);
 if (dup_value == NULL)
 {
-free(new_node);
-free(dup_key);
+free_node(new_node);
 return (0); } }
 else
 dup_value = NULL;
 new_node->key = dup_key;
 new_node->value = dup_value;
-new_node->next = NULL;
-
 index = key_index((unsigned char *)dup_key, ht->size);
-
 if (ht->array[index] != NULL)
 {
-if (strcmp(ht->array[index]->key, dup_key) == 0)
-{
-ht->array[index]->value = dup_value;
-free_node(new_node);
-return (1);
-}
 tmp = ht->array[index];
-while (tmp->next != NULL)
+while (tmp != NULL)
 {
 if (strcmp(tmp->key, dup_key) == 0)
 {
 tmp->value = dup_value;
 free_node(new_node);
-return (1);
-}
-tmp = tmp->next;
-}
-if (strcmp(tmp->key, dup_key) == 0)
-{
-tmp->value = dup_value;
-free_node(new_node);
-return (1);
-}
-}
-
+return (1); }
+tmp = tmp->next; } }
 new_node->next = ht->array[index];
 ht->array[index] = new_node;
-
-
-return (1);
-}
+return (1); }
