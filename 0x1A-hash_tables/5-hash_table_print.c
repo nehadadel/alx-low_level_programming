cec8a2ca0165488fde92c1b547d149b2d4ee0d3f
nehadadel <eng.nehadadel@gmail.com>
2023-12-01 18:55:22 -0800

    t

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ *hash_table_print -  prints a hash table.
+ *
+ *@ht: is the hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int i;
+hash_node_t *tmp;
+if (ht == NULL)
+{
+printf("{}\n");
+return; }
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+if (ht->array[i] != NULL)
+{
+tmp = ht->array[i];
+while (tmp != NULL)
+{
+printf("'%s': '%s'", tmp->key, tmp->value);
+if (tmp->next != NULL)
+printf(",");
+tmp = tmp->next;
+}
+}
+}
+printf("}\n");
+}
