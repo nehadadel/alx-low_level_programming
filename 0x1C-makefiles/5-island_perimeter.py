6f59767c46cb1e6ca7c7dd32e5fe7d5781ac8dc0
nehadadel <eng.nehadadel@gmail.com>
2024-01-24 13:45:24 -0800

    interview

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """that returns the perimeter of the island described in grid"""
+    prem = 0
+    flag_c = 0
+    flag_r = 0
+    flag = 1
+    for row in grid:
+        for coulom in row:
+            if coulom == 1:
+                prem += 4
+                flag_c += 1
+                flag = 1
+        prem = prem - (flag_c - 1) * 2
+        if flag == 1:
+            flag_r += 1
+        flag_c = 0
+    prem = prem - (flag_r - 1) * 2
+    return prem
